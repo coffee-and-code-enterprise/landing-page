eb9acf5fa6f0b6dd0419702e6f8822fc68e8faac
Emanuel Resende <emanuelaugustosantos13@gmail.com>
2025-09-01 16:49:35 -0300

    Home quase pronta

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
           <h1 className="hero-title">DESENVOLVEMOS SOLUÇÕES SOB MEDIDA, COM A
             <span> EFICIÊNCIA DO CÓDIGO </span>
             E O CONFORTO DE UMA BOA XÍCARA DE CAFÉ</h1>
-          <div class="scroll-down">⌄</div>
+          <div><a class="scroll-down" href="">⌄</a></div>
         </div>
 
       </section>
@@ -27,25 +27,38 @@ function Home() {
           <h2 class="code-title"><span>CÓDIGO</span> FEITO PARA VOCÊ</h2>
           <div class="code-text"><p>Cada linha do nosso código é pensada para atender às necessidades específicas do seu projeto, garantindo performance e escalabilidade sem perder a identidade da sua marca.</p>
           </div>
-          
+
 
 
 
         </div>
         <div class="humor">
-            <img id="humor" src="public/imgs/home/humor_e_piadas.png" alt="" />
-          </div>
+          <img id="humor" src="/imgs/home/humor_e_piadas.png" alt="" />
+        </div>
       </section>
       <section class="team-home">
         <div class="team-content">
           <h2 class="team-title">CONHEÇA A NOSSA <span>EQUIPE</span></h2>
         </div>
-        <div>
-          <img src="public/imgs/gui" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
+        <div class="team-names">
+          <div><img class="team-images" src="/imgs/home/gui.png" alt="" />
+          <h3>Guilherme</h3> </div>
+          <div><img class="team-images" src="/imgs/home/manu.png" alt="" />
+          <h3>Emanuel</h3> </div>
+          <div><img class="team-images" src="/imgs/home/vini.png" alt="" />
+          <h3>Vinícius</h3> </div>
+          <div><img class="team-images" src="/imgs/home/cazé.png" alt="" />
+          <h3>Gustavo</h3> </div> </div>
+        
+      </section>
+      <section class="contact-home">
+        <div class="contact-content">
+          <div class="contact-text"><h2 id="text"><span>CONTATE-NOS</span> AGORA PARA AGENDAR UMA REUNIÃO</h2></div>
         </div>
+        <Link to="/contact" class="contact-button">
+          Contatar
+        </Link>
+
       </section>
     </main>
 
