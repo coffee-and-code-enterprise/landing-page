edf2bae448587b6f1a6ec8b27b7168e50077421b
Emanuel Resende <emanuelaugustosantos13@gmail.com>
2025-09-01 13:25:16 -0300

    criei a branch ia e fiz algumas coisas

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,16 +6,19 @@ function Home() {
   return (
     <section id="home" className="hero">
       <div className="container">
-        <h1 className="hero-title">Soluções Web Sob Medida</h1>
+        <h1 className="hero-title">DESENVOLVEMOS SOLUÇÕES SOB MEDIDA, COM A 
+          <span> EFICIÊNCIA DO CÓDIGO </span> 
+          E O CONFORTO DE UMA BOA XÍCARA DE CAFÉ</h1>
         <p className="hero-subtitle">
           Desenvolvemos aplicações web de alta performance que impulsionam seu
           negócio. Combinamos design inovador com tecnologia de ponta para criar
           experiências digitais excepcionais.
         </p>
-        <Link to="/contact" className="cta-button">
-          Fale Conosco
-        </Link>
+        
       </div>
+      <Link to="/contact" className="cta-button">
+          Contatar
+        </Link>
     </section>
   );
 }
