f04d0727d299066099c5fa20668486be3622a77e
Emanuel Resende <emanuelaugustosantos13@gmail.com>
2025-09-01 15:07:26 -0300

    contuando a home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,26 +5,43 @@ import "./Home.css";
 function Home() {
   return (
     <main>
-    <section id="home" className="hero">
-      <div className="container">
-        <h1 className="hero-title">DESENVOLVEMOS SOLUÇÕES SOB MEDIDA, COM A 
-          <span> EFICIÊNCIA DO CÓDIGO </span> 
-          E O CONFORTO DE UMA BOA XÍCARA DE CAFÉ</h1>
-      <div class="scroll-down">⌄</div>  
-      </div>
-      
-    </section>
-    <section class="main-home">
+      <section id="home" className="hero">
+        <div className="container">
+          <h1 className="hero-title">DESENVOLVEMOS SOLUÇÕES SOB MEDIDA, COM A
+            <span> EFICIÊNCIA DO CÓDIGO </span>
+            E O CONFORTO DE UMA BOA XÍCARA DE CAFÉ</h1>
+          <div class="scroll-down">⌄</div>
+        </div>
+
+      </section>
+      <section class="main-home">
         <div class="main-content">
-          <br /> 
-        <h2>TECNOLOGIA QUE FUNCIONA, <span>EXPERIÊNCIA QUE ENCANTA</span> </h2>
+          <br />
+          <h2>TECNOLOGIA QUE FUNCIONA, <span>EXPERIÊNCIA QUE ENCANTA</span> </h2>
         </div>
         <div class="main-content"><p><span>Na Coffee & Code, </span> aliamos a lógica do desenvolvimento com o calor humano de uma boa conversa. Criamos sistemas, sites e soluções digitais sob medida, com foco em desempenho, usabilidade e simplicidade.</p>
         </div>
       </section>
+      <section class="code-home">
+        <div class="code-content">
+          <h2 class="code-title"><span>CÓDIGO</span> FEITO PARA VOCÊ</h2>
+          <div class="code-text"><p>Cada linha do nosso código é pensada para atender às necessidades específicas do seu projeto, garantindo performance e escalabilidade sem perder a identidade da sua marca.</p>
+          </div>
+          
+
+
+
+        </div>
+        <div class="humor">
+            <img id="humor" src="public/imgs/humor_e_piadas.png" alt="" />
+          </div>
+      </section>
+      <section class="team-home">
+        <div class="team-content"></div>
+      </section>
     </main>
-    
-    
+
+
   );
 }
 
