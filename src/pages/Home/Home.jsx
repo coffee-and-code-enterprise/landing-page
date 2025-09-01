058f2b0aacc5688abb8d28a305a84b71603a8c7f
Emanuel Resende <emanuelaugustosantos13@gmail.com>
2025-09-01 14:09:51 -0300

    mudando a home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,22 +4,27 @@ import "./Home.css";
 
 function Home() {
   return (
+    <main>
     <section id="home" className="hero">
       <div className="container">
         <h1 className="hero-title">DESENVOLVEMOS SOLUÇÕES SOB MEDIDA, COM A 
           <span> EFICIÊNCIA DO CÓDIGO </span> 
           E O CONFORTO DE UMA BOA XÍCARA DE CAFÉ</h1>
-        <p className="hero-subtitle">
-          Desenvolvemos aplicações web de alta performance que impulsionam seu
-          negócio. Combinamos design inovador com tecnologia de ponta para criar
-          experiências digitais excepcionais.
-        </p>
-        
+      <div class="scroll-down">⌄</div>  
       </div>
-      <Link to="/contact" className="cta-button">
-          Contatar
-        </Link>
+      
     </section>
+    <section class="main-home">
+        <div class="main-content">
+          <br /> 
+        <h2>TECNOLOGIA QUE FUNCIONA, <span>EXPERIÊNCIA QUE ENCANTA</span> </h2>
+        </div>
+        <div class="main-content"><p><span>Na Coffee & Code, </span> aliamos a lógica do desenvolvimento com o calor humano de uma boa conversa. Criamos sistemas, sites e soluções digitais sob medida, com foco em desempenho, usabilidade e simplicidade.</p>
+        </div>
+      </section>
+    </main>
+    
+    
   );
 }
 
